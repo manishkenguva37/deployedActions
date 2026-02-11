b051cc18d9b1e7c41cebe4048d38d0e84c92b667
manimoney468 <manimoney468@gmail.com>
2026-02-11 16:42:04 +0530

    the hello worlsd

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-// run `node index.js` in the terminal
 import express from "express";
 
 const app = express();
-const PORT = 8080;
-app.get('/', (req, res) => {
-    return res.json({ msg: 'Hello from the server' })
-})
 
-app.listen(PORT, () => {
-    console.log(`Server is up and runnincccg mannsndon PORT ${PORT}`)
-})
-console.log(`Hello Node.js v${process.versions.node}!`);
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from Vercel Express!" });
+});
+
+export default app;
