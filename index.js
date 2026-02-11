1e91147fd2053ad8c0ae68c6265786b1722f65c9
manimoney468 <manimoney468@gmail.com>
2026-02-11 16:47:09 +0530

    feat: innn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from Vercel Express!" });
+  res.json({ message: "Hello from Vercevvvl Express!" });
 });
 
 export default app;
